81d0670a637f8e9b8f2ec4668abda5d9d617ff61
Santiago Guzman <superteso567@hotmail.com>
2021-08-22 13:57:21 -0500

    interfaz principal e imagenes

--- src/renderer/frontend.js
+++ src/renderer/frontend.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',()=>{
-    document.getElementById('mensaje').innerHTML = 'Este es un mensaje insertado desde JS';
+    /* document.getElementById('mensaje').innerHTML = 'Este es un mensaje insertado desde JS';
 
     document.querySelector('#button').addEventListener('click',()=>{
         alert('esto es una mini Ventana');
-    });
+    }); */
 });
